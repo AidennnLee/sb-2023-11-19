ed914abf550d57f76f7f6a31a6544065e82c4ac7
Aidennn-Lee <yw010804@gmail.com>
2023-11-19 14:40:40 +0900

    setting

---
+++ src/main/java/com/ll/sb20231119/Sb20231119Application.java
@@ -0,0 +1,13 @@
+package com.ll.sb20231119;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sb20231119Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sb20231119Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/ll/sb20231119/Sb20231119ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.sb20231119;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sb20231119ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
